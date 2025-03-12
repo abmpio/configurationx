7be159ee36db993844f4ab377da83e300bbc9ac6
jeseeqing <jeseeqing@gmail.com>
2025-03-12 18:30:12 +0800

    feat: add Validate func in AliyunSmsOptions structure

--- options/aliyun/sms_options.go
+++ options/aliyun/sms_options.go
@@ -1,5 +1,7 @@
 package aliyun
 
+import "fmt"
+
 type AliyunSmsOptions struct {
 	// aksk
 	AccessKeyId     string `json:"accessKeyId" mapstructure:"accessKeyId"`
@@ -9,4 +11,17 @@ type AliyunSmsOptions struct {
 
 	// 不用于实际逻辑，仅仅只是用来表示这个aksk所对应的用户信息
 	UserInfo string `json:"userInfo" mapstructure:"userInfo"`
+}
+
+func (o *AliyunSmsOptions) Validate() error {
+	if len(o.AccessKeyId) <= 0 {
+		return fmt.Errorf("必须指定accessKeyId")
+	}
+	if len(o.AccessKeySecret) <= 0 {
+		return fmt.Errorf("必须指定accessKeySecret")
+	}
+	if len(o.DefaultSign) <= 0 {
+		return fmt.Errorf("必须指定defaultSign参数")
+	}
+	return nil
 }
